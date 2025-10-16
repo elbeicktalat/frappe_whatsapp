8fb3b64678f5615bf79eec494badb8702289ae2d
Talat El Beick <talatelbeick@gmail.com>
2025-10-17 00:53:36 +0300

    Update webhook.py

--- frappe_whatsapp/utils/webhook.py
+++ frappe_whatsapp/utils/webhook.py
@@ -3,6 +3,7 @@ import frappe
 import json
 import requests
 import time
+from datetime import datetime
 from werkzeug.wrappers import Response
 import frappe.utils
 
@@ -277,13 +278,11 @@ def update_message_status(data):
     status = status_info['status']
     conversation = status_info.get('conversation', {}).get('id')
 
-    # FIX: Convert the timestamp (which is in milliseconds) to seconds for Frappe's utility
-    timestamp_unix_ms = status_info.get('timestamp')
+    timestamp = status_info.get('timestamp')
 
-    if timestamp_unix_ms:
+    if timestamp:
         # Convert to integer and divide by 1000
-        timestamp_unix_s = int(timestamp_unix_ms) / 1000
-        timestamp_dt = frappe.utils.get_datetime(timestamp_unix_s)
+        timestamp_dt = datetime.fromtimestamp(timestamp)
     else:
         return  # Cannot update status without a timestamp
 
